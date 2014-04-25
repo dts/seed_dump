8f4ec779ad644320173b50ae1864109b21acf9f1
Daniel Staudigel <dstaudigel@gmail.com>
2014-04-24 23:07:59 -0700

    Use all models available from all engines, not just the local app's.

--- lib/seed_dump/environment.rb
+++ lib/seed_dump/environment.rb
@@ -1,5 +1,21 @@
 class SeedDump
   module Environment
+    def lchomp(base, arg)
+      base.to_s.reverse.chomp(arg.to_s.reverse).reverse
+    end
+
+    def possible_models
+      ([Rails.application] + Rails::Engine.subclasses.collect(&:instance)).flat_map do |app|
+        (app.paths['app/models'].to_a + app.config.autoload_paths).collect do |load_path|
+          Dir.glob(app.root.join(load_path)).collect do |load_dir|
+            Dir.glob(load_dir + '/**/*.rb').collect do |filename|
+              # app/models/module/class.rb => module/class.rb => module/class => Module::Class
+              lchomp(filename, "#{app.root.join(load_dir)}/").chomp('.rb').camelize
+            end
+          end
+        end
+      end.flatten.reject { |m| m.starts_with?('Concerns::') }.collect { |m| m.constantize }
+    end
 
     def dump_using_environment(env = {})
       Rails.application.eager_load!
@@ -7,7 +23,7 @@ class SeedDump
       models = if env['MODEL'] || env['MODELS']
                  (env['MODEL'] || env['MODELS']).split(',').collect {|x| x.strip.underscore.singularize.camelize.constantize }
                else
-                 ActiveRecord::Base.descendants.select do |model|
+                 possible_models.select do |model|
                    (model.to_s != 'ActiveRecord::SchemaMigration') && \
                     model.table_exists? && \
                     model.exists?
